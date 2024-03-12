b8f848efe4a0d46cc89b8c913d32512a75f4f534
alexbarasa <alexbarasa847@gmail.com>
2024-03-12 18:29:50 +0300

    Conditional statements, loops and functions

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     remainder = abs(number) % 10
-    if number < 0:
-        return(-(remainder))
-    else:
-        return(remainder)
+    return(remainder)
