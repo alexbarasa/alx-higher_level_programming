b410eae5d5fbd61105c13861b8de77a277087268
alexbarasa <alexbarasa847@gmail.com>
2024-03-12 12:22:01 +0300

    Conditional statements, loops and functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,11 +6,13 @@ if (number < 0):
     if (remainder == 0):
         print(f"Last digit of {number} is {remainder} and is 0")
     else:
-        print(f"Last digit of {number} is {-remainder} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {-remainder} and is"
+              " less than 6 and not 0")
 else:
     if (number % 10 == 0):
         print(f"Last digit of {number} is {remainder} and is 0")
     elif (number % 10 > 5):
         print(f"Last digit of {number} is {remainder} and is greater than 5")
     else:
-        print(f"Last digit of {number} is {remainder} and is less than 6 and not 0")
+        print(f"Last digit of {number} is {remainder} and is"
+              " less than 6 and not 0")
