a41fcb9e0114b9d80e4ee5f9d8a27d3ed819f545
alexbarasa <alexbarasa847@gmail.com>
2024-03-12 16:55:08 +0300

    Conditional statements, loops and functions

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,4 @@ def uppercase(str):
         if 'a' <= i <= 'z':
             upper = chr(ord(i) - ord('a') + ord('A'))
             print("{}".format(upper), end='')
-     print('\n')
+    print('\n')
