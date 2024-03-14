652befd2e6ed0dd31bf04d2074b142746a392575
alexbarasa <alexbarasa847@gmail.com>
2024-03-14 16:44:18 +0300

    Python import modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,13 +6,13 @@ def args():
 
     arg = sys.argv[1:]
     if len(arg) == 1:
-        print("{} argument: ".format(len(arg)))
+        print("{} argument:".format(len(arg)))
     elif len(arg) == 0:
-        print("{} argument. ".format(len(arg)))
+        print("{} argument.".format(len(arg)))
     else:
-        print("{} arguments ".format(len(arg)))
+        print("{} arguments".format(len(arg)))
     for i, j in enumerate(arg):
-        print("{}{} {} ".format(i + 1, ':', j))
+        print("{}: {}".format(i + 1, j))
 
 
 if __name__ == "__main__":
