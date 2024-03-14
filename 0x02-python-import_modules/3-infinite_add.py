62e05c2ba7d30f2b241661a3f521a055910ce2df
alexbarasa <alexbarasa847@gmail.com>
2024-03-14 17:11:20 +0300

    Python import modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 import sys
 
+
 def add_args():
 
     total = 0
     args = sys.argv[1:]
     for i in args:
-       total += int(i)
+        total += int(i)
     print(total)
 
 
 if __name__ == "__main__":
+
     add_args()
