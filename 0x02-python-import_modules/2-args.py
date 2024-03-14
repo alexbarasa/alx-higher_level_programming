699927c9519ad9031a3b2860e5b31bacb5123f90
alexbarasa <alexbarasa847@gmail.com>
2024-03-14 15:57:53 +0300

    Python import modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 import sys
 
-arg = sys.argv[1:]
-if len(arg) == 1:
-    print("{} argument".format(len(arg)))
-else:
-    print("{} arguments".format(len(arg)))
-for i, j in enumerate(arg):
-    print("{}{} {}".format(i + 1, ':', j))
-if __name__ == "main":
-    pass
+if __name__ == "__manin__":
+    arg = sys.argv[1:]
+    if len(arg) == 1:
+        print("{} argument".format(len(arg)))
+    else:
+        print("{} arguments".format(len(arg)))
+    for i, j in enumerate(arg):
+        print("{}{} {}".format(i + 1, ':', j))
