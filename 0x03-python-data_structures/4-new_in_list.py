d3f40f0a7ae506909238a041975d5f634dc75f21
alexbarasa <alexbarasa847@gmail.com>
2024-03-17 21:18:29 +0300

    Python data structures

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,10 +2,10 @@
 def new_in_list(my_list, idx, element):
 
     my_new_list = my_list
-    my_new_list[idx] = element
     if idx < 0:
-        return my_list
+        return my_new_list
     elif idx >= len(my_list):
-        return my_list
+        return my_new_list
     else:
+        my_new_list[idx] = element
         return my_new_list
