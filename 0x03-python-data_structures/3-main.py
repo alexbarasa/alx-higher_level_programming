565daacbe68ade599c6918facfd89f5ecd16bf53
alexbarasa <alexbarasa847@gmail.com>
2024-03-17 21:27:43 +0300

    Python data structures

---
+++ 0x03-python-data_structures/3-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
+
+my_list = [10, 11, 12, 13, '9',  1, 2, 3, 4, 5]
+print_reversed_list_integer(my_list)
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
 
-    my_new_list = my_list
+    my_new_list = my_list[:]
     if idx < 0:
         return my_new_list
     elif idx >= len(my_list):
