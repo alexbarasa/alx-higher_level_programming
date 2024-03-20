faed114473994148ed1f035e3db6f8515eaf74b7
alexbarasa <alexbarasa847@gmail.com>
2024-03-20 14:08:07 +0300

    Python more data structures: Sets and Dictionaries

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,7 +3,11 @@ def square_matrix_simple(matrix=[]):
 
     if not matrix:
         return
+
+    def square(x):
+
+        return lambda x: x**2
     new_matrix = matrix[:]
-    square = lambda x: x**2
-    S = list(map(lambda sublist: list(map(square, sublist)), new_matrix))
+    # square = lambda x: x**2
+    S = list(map(lambda lst: list(map(square(lst), lst)), new_matrix))
     return S
