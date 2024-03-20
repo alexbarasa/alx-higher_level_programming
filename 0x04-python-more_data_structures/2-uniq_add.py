03c46c3545385b09eeee5c460c6aa3ebce7b59ca
alexbarasa <alexbarasa847@gmail.com>
2024-03-20 18:54:23 +0300

    Python more data structures: Sets and Dictionaries

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
 
-    if not my_list:
-        return
     new = set()
     for i in my_list:
         new.add(i)
-    if not new:
-        return
     total = sum(new)
     return total
