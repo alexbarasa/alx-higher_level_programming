a055dc33e0318ec0586a40d0abf32336e82a37f3
alexbarasa <alexbarasa847@gmail.com>
2024-03-20 14:21:22 +0300

    Python more data structures: Sets and Dictionaries

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -4,7 +4,7 @@ def search_replace(my_list, search, replace):
     if not my_list:
         return
     new_list = my_list[:]
-    for in in new_list:
+    for i in new_list:
         if i == search:
             new_list[new_list.index(search)] = replace
     return new_list
