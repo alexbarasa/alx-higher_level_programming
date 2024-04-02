4fe83ef60a02c50309d5845113f31d4413e7c956
alexbarasa <alexbarasa847@gmail.com>
2024-04-02 13:33:30 +0300

    Python error handling using try/except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,10 @@ def safe_print_list(my_list=[], x=0):
         count = 0
         for i in my_list:
             count += 1
+        if x <= count:
+            return (x)
+        else:
+            return (count)
     except IndexError:
         print("Error occurred")
-    return (count)
+    #return (count)
