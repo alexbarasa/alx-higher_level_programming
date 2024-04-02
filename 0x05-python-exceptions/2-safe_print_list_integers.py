bbd62bf622a4893a1b0c2d2b36fb8ecc961b3839
alexbarasa <alexbarasa847@gmail.com>
2024-04-02 14:12:41 +0300

    Python error handling using try/except

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+
+    count = 0
+    try:
+        for i in my_list[:x]:
+            try:
+                print("{:d}".format(i), end='')
+                count += 1
+            except ValueError:
+                pass
+               # print("ValueError occured")
+        print()
+    except TypeError:
+        print()
+        # print("Error occured")
+        return (count)
+    return (count)
