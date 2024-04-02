9e1166ea4090ca52a0eb20849db45e8ecef347f9
alexbarasa <alexbarasa847@gmail.com>
2024-04-02 20:15:25 +0300

    Python error handling using try/except

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        # Attempt to convert the value to an integer
+        integer_value = int(value)
+        # Print the integer value followed by a newline
+        print("{:d}".format(integer_value))
+        return (True)
+    except ValueError:
+        # If conversion fails, print an error message to stderr
+        print("Exception: Unknown format code 'd' for object of type 'str'", file=sys.stderr)
+        return (False)
+    except Exception as e:
+        # If any other exception occurs, print an error message to stderr
+        print("Exception:", e, file=sys.stderr)
+        return (False)
