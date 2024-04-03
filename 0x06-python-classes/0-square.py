5b7619b8661c8fd2632442c3a6fc00df4fea36c1
alexbarasa <alexbarasa847@gmail.com>
2024-04-03 20:14:52 +0300

    Python classes

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+This is the square class which will have different methods
+Methods will be implemented later but this is just a prototype
+of square
+"""
 class Square:
     """
     A class representing a square.
